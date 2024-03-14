02707183afc011d8f2be57c228cb0af82c8283a4
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-03-14 15:45:07 -0500

    Generated Index page and implemented a simple search algorithm (still developing), Implemented design for the pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def index
+    @fave_dishes = FaveDish.includes(:country_dish).limit(10)
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def about
   end
+
+  def index
+    if params[:search].present?
+      # Implement multi purpose search logic
+      # Todo: implement logic for search country also
+      @fave_dishes = FaveDish.joins(country_dish: :dish)
+                             .where('LOWER(dishes.name) LIKE ?', "%#{params[:search].downcase}%")
+
+    else
+      # No search, just list favorite dishes
+      @fave_dishes = FaveDish.includes(:country_dish).limit(10)
+    end
+  end
 end
