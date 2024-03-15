74421077c40612da3d047c582b7e4cf144714bb9
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-03-15 05:05:53 -0500

    Fixed Search and Filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,21 +4,23 @@ class PagesController < ApplicationController
 
   def index
     @fave_dishes = FaveDish.includes(:country_dish).page(params[:page]).per(10)
-
     @regions = Country.distinct.pluck(:region)
+
+    if params[:region].present? && params[:region] != 'All Regions'
+      params[:search] = "" # resets so it queries once
+      @fave_dishes = @fave_dishes.joins(country_dish: :country)
+                                 .where('countries.region = ?', params[:region])
+
+    end
 
     if params[:search].present?  # && params[:region].present?
       # Implement multi purpose search logic
       # Todo: implement logic for search country also
-      @fave_dishes = FaveDish.joins(country_dish: :dish)
-                             .where(coountry_dish: { country: :dish })
-                             .page(params[:page]).per(10)
+      @fave_dishes = @fave_dishes.joins(country_dish: :dish)
+                                 .where('dishes.name LIKE ?', "%#{params[:search]}%")
+                                 .page(params[:page]).per(10)
     end
 
-    if params[:region].present? && params[:region] != 'All Regions'
-      # Join with countries to apply the region filter
-      @fave_dishes = @fave_dishes.joins(country_dish: :country)
-                                 .where('countries.region = ?', params[:region])
-    end
+
   end
 end
